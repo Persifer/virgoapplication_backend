0b02697ee1bec47028dd36f6718362ece1228c5f
Persifer <giorgino.antonio32@gmail.com>
2023-03-27 12:26:57 +0200

    First commit

---
+++ src/main/java/com/application/virgo/VirgoApplication.java
@@ -0,0 +1,13 @@
+package com.application.virgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/virgo/VirgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.virgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
