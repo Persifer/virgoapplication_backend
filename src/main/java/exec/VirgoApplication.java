d3c3fc393d04444053a2719d92407683754a7630
Persifer <giorgino.antonio32@gmail.com>
2023-03-31 11:31:40 +0200

    Changed application.properties into application.yml and created the connection between spring and the database.
    In pom.xml commented all the dependencies related to spring-security
    
    Created and setted the first entity of the virgo application

--- src/main/java/exec/VirgoApplication.java
+++ src/main/java/exec/VirgoApplication.java
@@ -1,9 +1,11 @@
-package com.application.virgo;
+package exec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.azienda.progettoFinale.model")
 public class VirgoApplication {
 
 	public static void main(String[] args) {
