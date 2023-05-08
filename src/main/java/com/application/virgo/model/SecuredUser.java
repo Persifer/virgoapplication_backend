5258df8d9765359e6dbd5b5db0b7b580c76d3b0c
Persifer <giorgino.antonio32@gmail.com>
2023-05-08 17:19:33 +0200

    Implemented the spring security dependency     in pom.xml Created the SecuredUser that is the Spring security UserDetails implementation for the entity Utente

---
+++ src/main/java/com/application/virgo/model/SecuredUser.java
@@ -0,0 +1,52 @@
+package com.application.virgo.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collection;
+
+public class SecuredUser implements UserDetails {
+
+    private final Utente utenteInformation;
+
+    public SecuredUser(Utente utenteInformation) {
+        this.utenteInformation = utenteInformation;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.utenteInformation.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.utenteInformation.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
