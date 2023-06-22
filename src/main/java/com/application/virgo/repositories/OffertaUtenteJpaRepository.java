c675de90b43388eaff745db99defa81a4cafa698
giuseppe713 <gabrypepp3@gmail.com>
2023-06-22 12:39:54 +0200

    aggiunta dei template di offerta più link necessari, sistemari errori di SQL

--- src/main/java/com/application/virgo/repositories/OffertaUtenteJpaRepository.java
+++ src/main/java/com/application/virgo/repositories/OffertaUtenteJpaRepository.java
@@ -54,10 +54,10 @@ public interface OffertaUtenteJpaRepository extends JpaRepository<OfferteUtente,
     public List<Long> getAllOfferteUtenteAsOfferente(@Param("idRequestedUtente") Long idOfferente);
 
     @Query(value = "select immobile.id_immobile from offerte_utente " +
-            "join offerta on (offerta_utente.id_offerta = offerta.id_offerta) " +
+            "join offerta on (offerte_utente.id_offerta = offerta.id_offerta) " +
             "join immobile on (offerta.id_immobile = immobile.id_immobile) " +
-            "where offerta_utente.id_proprietario = :idProprietario " +
-            "and offerta_utente.id_offerente=:idOfferente " +
+            "where offerte_utente.id_proprietario = :idProprietario " +
+            "and offerte_utente.id_offerente=:idOfferente " +
             "group by immobile.id_immobile;", nativeQuery = true)
     public List<Long> getImmobiliInteressati(@Param("idOfferente") Long idOfferente, @Param("idProprietario") Long idProrietario);
 
